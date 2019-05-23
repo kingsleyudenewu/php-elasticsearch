4ac7631b92add2bb75164c2f6eea8b88bd8e970d
= <=>
2019-05-23 14:47:23 +0100

    Added mapping method to specify the index and tables to map

--- config/Elastic.php
+++ config/Elastic.php
@@ -13,4 +13,77 @@ class Elastic
 		# code...
 		$this->elastic_client = Elasticsearch\ClientBuilder::create()->build();
 	}
+
+	public function Mapping(){
+	    $params = [
+	        'index' => 'products',
+	        'body' => [
+	            'mappings' => [
+	                'product' => [
+	                    'properties' => [
+	                        'id' => [
+	                            'type' => 'integer'
+	                         
+	                        ],
+	                        'title' => [
+	                            'type' => 'string'
+	                         
+	                        ],
+	                        'description' => [
+	                            'type' => 'string'
+	                         
+	                        ],
+	                        'sku' => [
+	                            'type' => 'string'
+	                         
+	                        ],
+	                        'quantity' => [
+	                            'type' => 'integer'
+	                         
+	                        ],
+	                        'bulk_quantity' => [
+	                            'type' => 'integer'
+	                         
+	                        ],
+	                        'warranty' => [
+	                            'type' => 'string'
+	                         
+	                        ],
+	                        'dimension' => [
+	                            'type' => 'string'
+	                         
+	                        ],
+	                        'weight' => [
+	                            'type' => 'string'
+	                         
+	                        ],
+	                        'barcode' => [
+	                            'type' => 'string'
+	                         
+	                        ],
+	                        'cat_name' => [
+	                            'type' => 'string'
+	                         
+	                        ],
+	                        'cat_description' => [
+	                            'type' => 'string'
+	                         
+	                        ],
+	                        'sub_category_name' => [
+	                            'type' => 'string'
+	                         
+	                        ],
+	                        'brand_name' => [
+	                            'type' => 'string'
+	                         
+	                        ],
+
+	                    ]
+	                ]
+	            ]
+	        ]
+	    ];
+       $this->elastic_client->indices()->create($params);
+       
+    }
 }
