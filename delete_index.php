511a1cd2e82fc5a788c5f9a4c40cc136fcb48ccc
= <=>
2019-05-23 18:17:26 +0100

    Refactored returned function for delete_node

--- delete_index.php
+++ delete_index.php
@@ -1,6 +1,6 @@
 <?php 
 require_once 'config/Elastic.php';
 if(isset($_REQUEST['q'])){
-	$insert_node = $elastic->search($_REQUEST['q']);
+	$insert_node = $elastic->delete_node($_REQUEST['q']);
 	echo $insert_node;
 }
